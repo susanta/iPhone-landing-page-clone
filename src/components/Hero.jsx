410dc773fb27edb41f9327aa7077393bbb78d8c6
Susanta <inbox.susanta@gmail.com>
2024-07-02 20:29:36 +0530

    Added action button to hero section & animate

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -27,8 +27,9 @@ const Hero = () => {
   useGSAP(() => {
     gsap.to('#hero', {
       opacity: 1,
-      delay: 1.5,
+      delay: 2,
     });
+    gsap.to('#cta', { opacity: 1, y: -50, delay: 2 });
   }, []);
 
   return (
@@ -48,6 +49,16 @@ const Hero = () => {
             <source src={videoSrc} type="video/mp4" />
           </video>
         </div>
+      </div>
+
+      <div
+        id="cta"
+        className="flex flex-col items-center opacity-0 translate-y-20"
+      >
+        <a href="#highlights" className="btn">
+          Buy
+        </a>
+        <p className="font-normal text-xl">From $199/moth or $999</p>
       </div>
     </section>
   );
