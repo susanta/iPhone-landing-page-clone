d7fdc15a3ab31668a48dce9752885969a9f6d9e8
Susanta <inbox.susanta@gmail.com>
2024-07-06 17:02:50 +0530

    Added basic structure on model view & lights.js

--- src/components/ModelView.jsx
+++ src/components/ModelView.jsx
@@ -1,5 +1,31 @@
-const ModelView = () => {
-  return <div>ModelView</div>;
+import { PerspectiveCamera, View } from '@react-three/drei';
+import Lights from './Lights';
+
+const ModelView = ({
+  index,
+  groupRef,
+  gsapType,
+  controlRef,
+  setRotationSize,
+  size,
+  item,
+}) => {
+  return (
+    <View
+      index={index}
+      id={gsapType}
+      className={`border-2 border-red-500 w-full h-full ${
+        index === 2
+      } ? 'right-[-100%] : ''`}
+    >
+      {/* Ambient Light */}
+      <ambientLight intensity={0.3} />
+
+      <PerspectiveCamera makeDefault position={[0, 0, 4]} />
+
+      <Lights />
+    </View>
+  );
 };
 
 export default ModelView;
