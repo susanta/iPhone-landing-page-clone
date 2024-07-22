47664382f603e0e0645c7cd4b048e7266c940aaa
Susanta <inbox.susanta@gmail.com>
2024-07-22 21:14:02 +0530

    Add features component & animate feature component heading

---
+++ src/components/Features.jsx
@@ -0,0 +1,25 @@
+import { useGSAP } from '@gsap/react';
+import { animateWithGsap } from '../utils/animations';
+
+const Features = () => {
+  useGSAP(() => {
+    animateWithGsap('#features_title', {
+      y: 0,
+      opacity: 1,
+    });
+  }, []);
+
+  return (
+    <section className="h-full common-padding bg-zinc relative overflow-hidden">
+      <div className="screen-max-width">
+        <div className="mb-12 w-full">
+          <h1 id="features_title" className="section-heading">
+            Explore then full story.
+          </h1>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Features;
