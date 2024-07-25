61e26fd3fee36473756c5ded9b30b6644a17de39
Susanta <inbox.susanta@gmail.com>
2024-07-25 22:26:49 +0530

    Added footer

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Navbar from './components/Navbar';
 import Model from './components/Model';
 import Features from './components/Features';
 import HowItWorks from './components/HowItWorks';
+import Footer from './components/Footer';
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
       <Model />
       <Features />
       <HowItWorks />
+      <Footer />
     </main>
   );
 };
