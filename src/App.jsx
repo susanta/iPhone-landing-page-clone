d74acbd696150d0f3f5f2ec8ebb3a304fdede5bd
Susanta <inbox.susanta@gmail.com>
2024-07-25 21:06:05 +0530

    Create HowItWorks basic component & animate chip image

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Highlights from './components/Highlights';
 import Navbar from './components/Navbar';
 import Model from './components/Model';
 import Features from './components/Features';
+import HowItWorks from './components/HowItWorks';
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
       <Highlights />
       <Model />
       <Features />
+      <HowItWorks />
     </main>
   );
 };
---
+++ src/components/HowItWorks.jsx
@@ -0,0 +1,30 @@
+import { useGSAP } from '@gsap/react';
+import { chipImg } from '../utils';
+import gsap from 'gsap';
+
+const HowItWorks = () => {
+  useGSAP(() => {
+    gsap.from('#chip', {
+      scrollTrigger: {
+        trigger: '#chip',
+        start: '20% bottom',
+      },
+      opacity: 0,
+      scale: 2,
+      duration: 2,
+      ease: 'power2.inOut',
+    });
+  }, []);
+
+  return (
+    <section className="common-padding">
+      <div className="screen-max-width">
+        <div id="chip" className="flex-center w-full mt-20">
+          <img src={chipImg} alt="chip" width={180} height={180} />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default HowItWorks;
